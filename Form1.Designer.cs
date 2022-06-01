5bdb88a29b969df91d24f6ca5b8f7ab0cc490568
zoranvujanic <vujanic.zoran007@gmail.com>
2022-06-01 18:33:31 +0200

    Prvi dan

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,48 @@
+using System;
+using System.Diagnostics;
+
+
 namespace WinFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        Stopwatch sw = new Stopwatch();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sw.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sw.Stop();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+    enum Smer
+    {
+        DESNO,
+        DOLE,
+        LEVO,
+        GORE
+    }
+    
+    public class Pacman
+    {
+        int x;
+        int y;
+        Smer smer;
+        float brizina;
     }
 }
