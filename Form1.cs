a01de77ffaf017392a6f7608020187d5110d6689
zoranvujanic <vujanic.zoran007@gmail.com>
2022-06-08 18:45:33 +0200

    AAAAAAAAAAAAAAAAAAAAAA

--- Form1.cs
+++ Form1.cs
@@ -20,13 +20,12 @@ namespace WinFormsApp1
         float lastTime;
         Random random = new Random();
 
-        const float size = 50.0f;
         int trenugao = 90;
 
-        TexturedQuad kvadrat = new TexturedQuad(0, 0, 0.97f * size, 0.97f * size, "../../../res/pacman.png");
+        TexturedQuad kvadrat = new TexturedQuad(0, 0, 0.98f * Globals.size, 0.98f * Globals.size, "../../../res/pacman.png");
         //Quad kvadrat2 = new Quad(100, 200, 100, 100, Color.Black);
         List<Quad> zidovi = new List<Quad>();
-        List<Quad> coini = new List<Quad>();
+        List<Circle> coini = new List<Circle>();
 
         Smer smer = Smer.DESNO;
         Smer bufferedSmer = Smer.DESNO;
@@ -59,16 +58,16 @@ namespace WinFormsApp1
                 {
                     if (mapa[i, j] == 'p')
                     {
-                        kvadrat.X = j * size;
-                        kvadrat.Y = i * size;
+                        kvadrat.X = j * Globals.size;
+                        kvadrat.Y = i * Globals.size;
                     }
                     else if (mapa[i, j] == '#')
                     {
-                        zidovi.Add(new Quad(j * size, i * size, size, size, Color.Blue));
+                        zidovi.Add(new Quad(j * Globals.size, i * Globals.size, Globals.size, Globals.size, Color.Blue));
                     }
                     else if (mapa[i, j] == ' ')
                     {
-                        coini.Add(new Quad(j * size, i * size, size, size, Color.Yellow));
+                        coini.Add(new Circle(j * Globals.size, i * Globals.size, Globals.size /2, Color.Yellow));
                     }
                 }
             }
@@ -94,17 +93,26 @@ namespace WinFormsApp1
         }
         bool Collision(TexturedQuad a, Quad b)
         {
-            /*
-            return (((a.X + a.W >= b.X) && (a.X + a.W <= b.X + b.W))
-                || ((a.X <= b.X + b.W) && (a.X >= b.X)))
-                && (((a.Y + a.H >= b.Y) && (a.Y + a.H <= b.Y + b.H))
-                || ((a.Y <= b.Y + b.H) && (a.Y >= b.Y)));
-            */
-            return (((a.X + a.W > b.X) && (a.X + a.W <= b.X + b.W))
-               || ((a.X < b.X + b.W) && (a.X > b.X)))
-               && (((a.Y + a.H > b.Y) && (a.Y + a.H <= b.Y + b.H))
-               || ((a.Y < b.Y + b.H) && (a.Y > b.Y)));
+
+           //return (((a.X + a.W > b.X) && (a.X + a.W <= b.X + b.W))
+           //   || ((a.X < b.X + b.W) && (a.X > b.X)))
+           //   && (((a.Y + a.H > b.Y) && (a.Y + a.H <= b.Y + b.H))
+           //   || ((a.Y < b.Y + b.H) && (a.Y > b.Y)));
+
+            return (((Helpers.Snap(a.X) + a.W > b.X) && (Helpers.Snap(a.X) + a.W <= b.X + b.W))
+                || ((Helpers.Snap(a.X) < b.X + b.W) && (Helpers.Snap(a.X) > b.X)))
+                && (((Helpers.Snap(a.Y) + a.H > b.Y) && (Helpers.Snap(a.Y) + a.H <= b.Y + b.H))
+                || ((Helpers.Snap(a.Y) < b.Y + b.H) && (Helpers.Snap(a.Y) > b.Y)));
         }
+        bool Collision(TexturedQuad a, Circle b)
+        {
+
+            return (((Helpers.Snap(a.X) + Helpers.Snap(a.W) > Helpers.Snap(b.X) && (Helpers.Snap(a.X) + Helpers.Snap(a.W) <= Helpers.Snap(b.X + 2 * b.R)))
+               || ((Helpers.Snap(a.X) < Helpers.Snap(b.X + 2 * b.R)) && (Helpers.Snap(a.X) > Helpers.Snap(b.X))))
+               && (((Helpers.Snap(a.Y) + Helpers.Snap(a.H) > Helpers.Snap(b.Y)) && (Helpers.Snap(a.Y) + Helpers.Snap(a.H) <= Helpers.Snap(b.Y + 2 * b.R))))
+               || ((Helpers.Snap(a.Y) < Helpers.Snap(b.Y + 2 * b.R)) && (Helpers.Snap(a.Y) > Helpers.Snap(b.Y))));
+        }
+
         public void GameUpdate()
         {
             //kvadrat.image.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
@@ -235,7 +243,7 @@ namespace WinFormsApp1
         public void Draw(Graphics g)
         {
             Brush brush = new SolidBrush(Color);
-            g.FillRectangle(brush, (int)X, (int)Y, (int)W, (int)H);
+            g.FillRectangle(brush, (int)Helpers.Snap(X), (int)Helpers.Snap(Y), (int)Helpers.Snap(W), (int)Helpers.Snap(H));
             brush.Dispose();
         }
 
@@ -250,11 +258,6 @@ namespace WinFormsApp1
         public float Y { get; set; }
         public float W { get; set; }
         public float H { get; set; }
-        //private float _X, _Y, _W, _H;
-        //public float X { get { return _X; } set { _X = value; image.Location = new Point((int)X, (int)Y); } }
-        // public float Y { get { return _Y; } set { _Y = value; image.Location = new Point((int)X, (int)Y); } }
-        //public float W { get { return _W; } set { _W = value; image.Size = new Size((int)W, (int)H); } }
-        //public float H { get { return _H; } set { _H = value; image.Size = new Size((int)W, (int)H); } }
 
         public TexturedQuad(float _x, float _y, float _w, float _h, string _texture)
         {
@@ -273,8 +276,8 @@ namespace WinFormsApp1
 
         public void UpdateLocationAndSize()
         {
-            image.Location = new Point((int)X, (int)Y);
-            image.ClientSize = new Size((int)W, (int)H);
+            image.Location = new Point((int)Helpers.Snap(X), (int)Helpers.Snap(Y));
+            image.ClientSize = new Size((int)W,(int)H);
         }
 
     }
@@ -282,9 +285,9 @@ namespace WinFormsApp1
 
     public class Circle
     {
-        private float X { get; set; }
-        private float Y { get; set; }
-        private float R { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float R { get; set; }
         private Color Color { get; set; }
         public Circle(float _x, float _y, float _r, Color _color)
         {
@@ -296,10 +299,25 @@ namespace WinFormsApp1
         public void Draw(Graphics g)
         {
             Brush brush = new SolidBrush(Color);
-            g.FillEllipse(brush, (int)X, (int)Y, (int)2 * R, (int)2 * R);
+            g.FillEllipse(brush, (int)Helpers.Snap(X), (int)Helpers.Snap(Y), (int)2 * R, (int)2 * R);
             brush.Dispose();
         }
 
 
+    }
+
+    public static class Globals
+    {
+        public const float size = 50.0f;
+
+
+    }
+    public static class Helpers
+    {
+        public static float Snap(float a, float val = Globals.size)
+        {
+            return a;
+            //return (int)(((int)a / (int)val) * val);
+        }
     }
 }
